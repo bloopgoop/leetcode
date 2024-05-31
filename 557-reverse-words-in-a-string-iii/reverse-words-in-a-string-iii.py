fe3c8d52a488fa984fd4e158d59a39fb3ef5c79d
bloopgoop <104113781+bloopgoop@users.noreply.github.com>
2024-05-30 21:46:12 -0400

    Time: 10 ms (97.07%) | Memory: 13 MB (25.18%) - LeetSync

---
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        words = s.split()
+        return " ".join([word[::-1] for word in words])  # join all items in iterable with " " between them
+        
