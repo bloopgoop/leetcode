48b4bc362207538823f53f747455c8a60a8299a7
bloopgoop <104113781+bloopgoop@users.noreply.github.com>
2024-05-30 22:28:20 -0400

    Time: 1377 ms (5.41%) | Memory: 12 MB (40.54%) - LeetSync

--- 2320-find-all-k-distant-indices-in-an-array/find-all-k-distant-indices-in-an-array.py
+++ 2320-find-all-k-distant-indices-in-an-array/find-all-k-distant-indices-in-an-array.py
@@ -6,13 +6,16 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
-        res = set()
-
-        # find all keys
+        res = []
         for i in range(len(nums)):
             if nums[i] == key:
                 for j in range(i - k, i + k + 1):
-                    if 0 <= j <= len(nums) - 1:
-                        res.add(j)
-        return sorted(res)
+                    if (0 <= j <= len(nums) - 1) and (len(res) == 0):
+                        res.append(j)
+                    if (0 <= j <= len(nums) - 1) and (j > res[-1]):
+                        res.append(j)
+
+        return res
                 
+                
+        
