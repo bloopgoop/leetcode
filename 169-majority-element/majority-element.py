b34cc0d3dd76ff399e947d8ae99085c2e1796747
Kevin Zhu <104113781+bloopgoop@users.noreply.github.com>
2025-06-03 02:31:35 -0400

    Time: 5 ms (65.66%) | Memory: 19.3 MB (87.46%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,4 +1,14 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums.sort()
-        return nums[len(nums) // 2]
+        # Boyer–Moore_majority_vote_algorithm
+        m = nums[0]
+        c = 0
+        for x in nums:
+            if c == 0:
+                m = x
+                c = 1
+            elif m == x:
+                c += 1
+            else:
+                c -= 1
+        return m
