dcc7308b34daa866b47b3b881bb576d64d01969b
Kevin Zhu <104113781+bloopgoop@users.noreply.github.com>
2025-06-03 17:11:47 -0400

    Time: 0 ms (100.00%) | Memory: 18.9 MB (44.56%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -1,26 +1,10 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        # buy and sell immediately if nums[i] > nums[i - 1]
         res = 0
-        l_min = prices[0]
-        l_max = prices[0]
 
-        # first pass, find local min and maxes
-        for price in prices:
-            if price < l_max:
-                # l_max was the local max, sell the stock
-                res += l_max - l_min
-                l_max = price
-                l_min = price
-            else:
-                l_max = price
-            
-            if price > l_min:
-                # l_min was the local min
-                pass
-            else:
-                l_min = price
-
-        if l_max > l_min:
-            res += l_max - l_min
-
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i - 1]:
+                res += prices[i] - prices[i - 1]
+        
         return res
