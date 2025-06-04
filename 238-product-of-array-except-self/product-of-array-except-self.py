577d015db277394e70d2ddac8b7dd7500634cf24
Kevin Zhu <104113781+bloopgoop@users.noreply.github.com>
2025-06-03 22:43:22 -0400

    Time: 22 ms (67.18%) | Memory: 23.3 MB (79.64%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,21 +1,15 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        left_to_right_products = [nums[0] for _ in range(len(nums))]
-        right_to_left_products = [nums[-1] for _ in range(len(nums))]
+        res = [1] * len(nums)
 
-        for i in range(1, len(nums)):
-            left_to_right_products[i] = left_to_right_products[i - 1] * nums[i]
-        
-        for i in range(len(nums) - 2, -1, -1):
-            right_to_left_products[i] = right_to_left_products[i + 1] * nums[i]
-
-        res = [0 for _ in range(len(nums))]
+        left = 1
         for i in range(len(nums)):
-            if i == 0:
-                res[i] = right_to_left_products[i + 1]
-            elif i == len(nums) - 1:
-                res[i] = left_to_right_products[i - 1]
-            else:
-                res[i] = left_to_right_products[i - 1] * right_to_left_products[i + 1]
+            res[i] *= left
+            left *= nums[i]
+
+        right = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= right
+            right *= nums[i]
 
         return res
